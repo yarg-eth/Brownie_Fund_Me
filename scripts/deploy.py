e7f3c2059d7802cd1b276ec860001b5e8fea1d96
Gray Blanchard <gblanchard679@gmail.com>
2022-03-22 23:16:03 -0500

    Add files via upload

---
+++ scripts/deploy.py
@@ -0,0 +1,10 @@
+from brownie import FundMe
+from scripts.helpful_scripts import get_account
+
+def deploy_fund_me():
+    account = get_account()
+    fund_me = FundMe.deploy({"from": account})
+    print(f"Contract deployed to {fund_me.address}"), publish_source=True
+
+def main():
+    deploy_fund_me()
